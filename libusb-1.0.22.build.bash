c27a588ec9e44302bd01aa598acbf9aae1b579c7
Arturo Rinaldi <arty.net2@gmail.com>
2018-05-24 01:09:27 +0200

    libusb : adding release v1.0.22 build

---
+++ libusb-1.0.22.build.bash
@@ -0,0 +1,38 @@
+#!/bin/bash -ex
+# Copyright (c) 2014-2016 Arduino LLC
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+mkdir -p objdir/$ARCH
+cd objdir/$ARCH
+PREFIX=`pwd`
+cd -
+
+if [[ ! -f libusb-1.0.22.tar.bz2  ]] ;
+then
+	wget http://download.sourceforge.net/project/libusb/libusb-1.0/libusb-1.0.22/libusb-1.0.22.tar.bz2
+fi
+
+tar xfv libusb-1.0.22.tar.bz2
+
+cd libusb-1.0.22
+CONFARGS="--prefix=$PREFIX --disable-udev --enable-static --disable-shared"
+if [[ $CROSS_COMPILE != "" ]] ; then
+  CONFARGS="$CONFARGS --host=$CROSS_COMPILE_HOST"
+fi
+CFLAGS="-w -O2 $CFLAGS" CXXFLAGS="-w -O2 $CXXFLAGS" LDFLAGS="-s $LDFLAGS" ./configure $CONFARGS
+make -j 1
+make install
+cd ..
